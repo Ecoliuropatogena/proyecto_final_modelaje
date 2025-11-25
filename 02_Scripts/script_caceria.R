ed53e8a7571546b959f3add9a8d022ac06d934b3
alomichdomg <adominguez09@alumnos.uaq.mx>
2025-11-24 22:04:19 -0600

    ...

--- 02_Scripts/script_caceria.R
+++ 02_Scripts/script_caceria.R
@@ -252,4 +252,29 @@ if(max_real.1 < 0) {
   print(paste("RESULTADO: El sistema sigue INESTABLE. Max Eigenvalor:", max_real.1))
 }
 
+# ----------------------------------------------------------------------------------
+# calculo del R0 #
 
+
+# Convertir los parámetros a lista para usar pars$parametro
+parametros.ref <- as.list(parametros.ref)
+
+# ------------------------------
+# Cálculo del R0 paso a paso
+# ------------------------------
+
+# 1. Fuerza total de infección (todos los b1x y b2x)
+beta_total <- parametros.ref$b11 + parametros.ref$b12 + parametros.ref$b13 + parametros.ref$b14 +
+  parametros.ref$b21 + parametros.ref$b22 + parametros.ref$b23 + parametros.ref$b24
+
+# 2. Tasa total de salida de los infectados (recuperación + muerte)
+salida_total <- parametros.ref$k1 + parametros.ref$k2 + parametros.ref$k3 + parametros.ref$a4
+
+# 3. Corrección por entierro seguro
+ajuste_entierro <- 1 - parametros.ref$ent
+
+# 4. Cálculo final del R0
+R0 <- beta_total / salida_total * ajuste_entierro
+
+# Mostrar resultado
+R0
